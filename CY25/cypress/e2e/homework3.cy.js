9701b0bc1da8cf3a501f08c34406f816768f8f7f
github lmmehri <github account lmmehri@gmail.com>
2024-12-29 22:46:43 -0500

    part of the assignment

--- CY25/cypress/e2e/homework3.cy.js
+++ CY25/cypress/e2e/homework3.cy.js
@@ -6,5 +6,35 @@ describe('Test confirm', () => {
       cy.on("window:alert",(str)=>{
         expect(str).to.be.equal("Hello , Are you sure you want to confirm?")
       })
-    })
+      
+  })
+  it.only('Test Table',() =>{
+    cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
+    cy.get('.tableFixHead > #product > tbody > :nth-child(5) > :nth-child(1)').each(($el,$index, $list ) =>{
+      const txt =$el.text()
+      if(txt.includes('Jack')){
+        cy.get('.tableFixHead > #product > tbody > :nth-child(5) > :nth-child(1)').eq($index).next().then(function(positionIndex) {
+         const position = positionIndex.text() 
+         
+         expect(position).to.be.equal('Engineer') 
+        
+         //cy.log(25)
+      }) 
+      cy.get('.tableFixHead > #product > tbody > :nth-child(5) > :nth-child(2)').eq($index).next().then(function(cityIndex) {
+        const city = cityIndex.text() 
+        
+        expect(city).to.be.equal('Pune') 
+       
+        //cy.log(25)
+     }) 
+     cy.get('.tableFixHead > #product > tbody > :nth-child(5) > :nth-child(3)').eq($index).next().then(function(amountIndex) {
+        const amount = amountIndex.text() 
+        
+        expect(amount).to.be.equal('32') 
+
+     })     
+    
+      }
+  })
+})
 })
--- CY25/cypress/e2e/lab4.cy.js
+++ CY25/cypress/e2e/lab4.cy.js
@@ -1,5 +1,5 @@
 describe('Test Alert,Table andMouse hover', () => {
-    it.only('Test Alert',() => {
+    it('Test Alert',() => {
       cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
       cy.get('#alertbtn').click()
 
@@ -7,19 +7,25 @@ describe('Test Alert,Table andMouse hover', () => {
         expect(str).to.be.equal("Hello , share this practice page and share your knowledge")
       })
     });
-    it('Test Table',() =>{
+    it.only('Test Table',() =>{
       cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
       cy.get('tr td:nth-child(2)').each(($el,$index, $list ) =>{
         const txt =$el.text()
-        if(txt.includes('Receptionist')){
-            cy.get("tr td:nth-child(2)").eq($index).next().then(function(name){
-               // const p =City.text()
-                expect(p).to.equal(25)
-            })
-       }
+        if(txt.includes('Python')){
+            cy.get("tr td:nth-child(2)").eq($index).next().then(function(price) {
+           const p = price.text() 
+           
+           expect(p).to.be.equal('25') 
+           //cy.log(25)
+        }) 
+        cy.get("tr td:nth-child(2)").eq($index).prev().then(function(price) {
+            const p = price.text()  
+           // expect(p).to.be.equal('25') 
+            cy.log(p)   
+       })
       
-      })
+        }
     });
+})
+})
     //it('Test mouse hover( =>))
-
-    })
